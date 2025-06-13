1f27717b34894fcc34489c943e35be9bf9ba3b17
PunitKumar-S <punitkumarpb604@gmail.com>
2025-06-13 12:18:14 +0530

    Created a User Form

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/test', (req, res)=>{
-    res.send('user Test Route')
+router.get('/register', (req, res)=>{
+    res.render('register')
 })
-
-
 
 module.exports = router;
