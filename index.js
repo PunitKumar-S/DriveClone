4f05b6310a53e393311e1e0a79168966b1460bf8
PunitKumar-S <punitkumarpb604@gmail.com>
2025-06-14 11:10:53 +0530

    Setting up Express-Validator

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const userRouter = require('./routes/user.routes')
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.set('view engine', 'ejs');
 
 app.use('/user', userRouter);
+
 
 // JWT - Json Web Token
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+// express validators
+const { body, validationResult } = require('express-validator')
+
+
+/*
+'a@a.com'
+''
+*/
 
 
 router.get('/register', (req, res)=>{
     res.render('register')
 })
+
+router.post('/register',     
+    body('email').trim().isEmail().isLength({min: 13}),
+    body('password').trim().isLength({min: 5}),
+    body('username').trim().isLength({min: 3}),
+    (req, res)=>{
+        
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid Data"
+            })
+        }
+    })
 
 module.exports = router;
